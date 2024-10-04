b980722f775b527689fc2727cb13e00aa13930d4
iakmorales <zaskiamaulidina12@gmail.com>
2024-10-04 21:20:57 +0700

    tahun kabisat

--- pemilihan2percobaan124.java
+++ pemilihan2percobaan124.java
@@ -10,8 +10,8 @@ public static void main(String[] args) {
     int tahun = input24.nextInt();
 
     //if ((tahun % 4 == 0) && (tahun % 100  != 0 || tahun % 400 == 0)) {
-       if ((tahun % 4 == 0)) {
-       if ((tahun % 100 ) != 0)
+      if ((tahun % 4 == 0)) {
+      if ((tahun % 100 ) == 0)
            System.out.println("Tahun Kabisat"); 
 }   else 
        System.out.println("Bukan Tahun Kabisat ");
